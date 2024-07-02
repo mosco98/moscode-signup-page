f6e0d08b987ec77069c9f78f56ad00f30963bf22
Moses Enyinnaya <moscolyno98@gmail.com>
2024-07-02 22:57:07 +0200

    fixed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,13 @@
 import { geist } from "@/lib/font";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false
+};
 
 export const metadata: Metadata = {
   title: "Create account",
